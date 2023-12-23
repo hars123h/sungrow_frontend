9e4ab3cc0c105f373b12a35f04e82c9f8b1238cd
Raman Madhukar <146058279+007raman@users.noreply.github.com>
2023-12-23 10:48:35 +0530

    bank card

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -49,7 +49,7 @@ const Popup = () => {
                             </div>
                         </div>
 
-                        <Link to={'https://t.me/Sungrow1'} className="flex justify-center items-end">
+                        <Link to={'https://t.me/TSMCOfficialChannel'} className="flex justify-center items-end">
                             <div>
                                 <img src={telegram} alt="telegram" className='w-12 m-1' />
                                 <p className='text-[#818393] text-base'>Telegram</p>
